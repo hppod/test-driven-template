75ffa6eea5c30b6fbb3e0bbae2b3dd09f022dc39
hppod <hppodgornik@gmail.com>
2018-12-01 16:44:10 -0200

    implementação do metodo divide na classe calculadora

--- src/tdt/calculadora/teste/Calculadora.java
+++ src/tdt/calculadora/teste/Calculadora.java
@@ -5,12 +5,16 @@ public class Calculadora {
 	public int soma(int valorA, int valorB) {
 		return valorA + valorB;
 	}
-	
+
 	public int subtrai(int valorA, int valorB) {
 		return valorA - valorB;
 	}
-	
+
 	public int multiplica(int valorA, int valorB) {
 		return valorA * valorB;
+	}
+
+	public int divide(int valorA, int valorB) {
+		return valorA / valorB;
 	}
 }
