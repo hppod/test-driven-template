c05391d455cf09f09cd93910703f021d676f3393
hppod <hppodgornik@gmail.com>
2018-12-01 16:17:55 -0200

    JUnit configurado

--- src/teste/junit/ConfigJUnit.java
+++ src/teste/junit/ConfigJUnit.java
@@ -1,5 +1,12 @@
 package teste.junit;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class ConfigJUnit {
 
+	@Test
+	public void testeConfiguracao() {
+		Assert.assertEquals(2, (1 + 1), 0);
+	}
 }
