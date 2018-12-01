2820425c8ae5e62e617538fb3b2b305b15130b39
hppod <hppodgornik@gmail.com>
2018-12-01 16:32:10 -0200

    implementando o metódo. erros nos testes

--- src/tdt/calculadora/teste/CalculadoraTeste.java
+++ src/tdt/calculadora/teste/CalculadoraTeste.java
@@ -1,12 +1,17 @@
 package tdt.calculadora.teste;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculadoraTeste {
-	
+
 	@Test
-	public void deveriaSomarDoisValores() throws Exception{
-		
+	public void deveriaSomarDoisValores() throws Exception {
+		int valorA = 1;
+		int valorB = 2;
+		int soma = 0;
+
+		assertEquals(3, soma);
 	}
 
 }
