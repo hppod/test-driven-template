7e04187c74c55d8a84f0dce5034a5cb559c4464b
hppod <hppodgornik@gmail.com>
2018-12-01 16:38:31 -0200

    implementação do metodo subtrai na classe calculadora

--- src/tdt/calculadora/teste/Calculadora.java
+++ src/tdt/calculadora/teste/Calculadora.java
@@ -5,4 +5,8 @@ public class Calculadora {
 	public int soma(int valorA, int valorB) {
 		return valorA + valorB;
 	}
+	
+	public int subtrai(int valorA, int valorB) {
+		return valorA - valorB;
+	}
 }
